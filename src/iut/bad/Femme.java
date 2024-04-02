62c28b187c2444de8370eca4af6ff581cf5e425d
Fitahiana <rakotosalamafitahiana@gmail.com>
2024-04-02 15:06:38 +0300

    Création de la classe Femme

---
+++ src/iut/bad/Femme.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iut.bad;
+
+/**
+ *
+ * @author Fitahiana
+ */
+public class Femme {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Femme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+}
